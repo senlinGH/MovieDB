d68c5d89981371f38a03047dac38bcf616d894a1
senlin32 <senlin32@hotmail.com>
2021-03-08 23:38:09 +0800

    ＊新增畫面＊ 1. 增加頂部圖片橫向滑動自動輪播功能

--- MovieDB/Controller/DiscoverViewController.swift
+++ MovieDB/Controller/DiscoverViewController.swift
@@ -19,9 +19,13 @@ class DiscoverViewController: UIViewController, UITableViewDelegate, UITableView
     var topRateData = [MovieDetailData]()
     var upcomingData = [MovieDetailData]()
     
+    let sliderImageArr = ["SliderImage1", "SliderImage2", "SliderImage3"]
+    var timer: Timer?   //計時器
+    var counter = 0 // 計數器
+    var randomNumArr = [Int]()
     
-    
-    
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var myTableView: UITableView!
     
     override func viewDidLoad() {
@@ -38,6 +42,16 @@ class DiscoverViewController: UIViewController, UITableViewDelegate, UITableView
         fetchTopRateData(page: 1)
         fetchUpcomingData(page: 1)
         
+        // 設定SliderCollectionViewFlowLayout
+        setSliderCollectionViewFlowLayout()
+        
+        // 設定Page Control
+        pageControl.numberOfPages = sliderImageArr.count
+        pageControl.currentPage = 0
+        
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
         
         
     }
@@ -185,6 +199,76 @@ extension DiscoverViewController: selectedCollectionItemDelegate {
         destinationVC.data = fetchResultData[index]
         navigationController?.pushViewController(destinationVC, animated: true)
     }
+    
+    
+}
+
+extension DiscoverViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sliderImageArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell  = sliderCollectionView.dequeueReusableCell(withReuseIdentifier: "sliderCell", for: indexPath) as! SliderCollectionCell
+        cell.sliderImage.image = UIImage(named: sliderImageArr[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    func setSliderCollectionViewFlowLayout() {
+        let layout = sliderCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.scrollDirection = .horizontal    // 設定水平滑動
+        
+        // section與section之間的距離(如果只有一個section，可以想像成frame)
+        layout?.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        let screenWidth = UIScreen.main.bounds.width    // 取得螢幕寬度
+        layout?.itemSize = CGSize(width: screenWidth, height: screenWidth / 2) // Cell的寬、高
+        
+        // 滑動方向為「垂直」的話即「上下」的間距;滑動方向為「平行」則為「左右」的間距
+        layout?.minimumLineSpacing = 0
+        sliderCollectionView.delegate = self
+        sliderCollectionView.dataSource = self
+        sliderCollectionView.showsHorizontalScrollIndicator = false //隱藏水平滑動bar
+        
+        // 動態調整HeaderView的高度
+        myTableView.tableHeaderView?.frame.size.height = screenWidth / 2
+        
+    }
+    
+    @objc func changeImage() {
+        if counter < sliderImageArr.count {
+            let index = IndexPath.init(item: counter, section: 0)
+            sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            pageControl.currentPage = counter
+            counter += 1
+        } else {
+            counter = 0
+            let index = IndexPath.init(item: counter, section: 0)
+            sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            pageControl.currentPage = counter
+            counter = 1
+            
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        pageControl.currentPage = indexPath.row
+    }
+    
+    // 離開畫面停止Timer
+    // Timer 如同執行緒，若沒停止它的話，它是會一直在背景執行的。所以必須在離開畫面時去停止它。
+    override func viewDidDisappear(_ animated: Bool) {
+        //  將timer執行緒停止
+        if self.timer != nil {
+            self.timer?.invalidate() //停止timer的循環
+        }
+    }
+    
+    
+    
     
     
 }
---
+++ MovieDB/View/DiscoverView/CollectionViewCell/SliderCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  SliderCollectionCell.swift
+//  MovieDB
+//
+//  Created by Lin Yi Sen on 2021/3/8.
+//  Copyright © 2021 Ethan. All rights reserved.
+//
+
+import UIKit
+
+class SliderCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sliderImage: UIImageView!
+}
